0710e3179cbb5c2a4f8fee0d9c2fa47aeb4e3a41
MKemp <mkemp@accu-time.com>
2020-12-27 13:39:24 -0500

    Write test class for MyCalc.kt

---
+++ app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
@@ -0,0 +1,46 @@
+package com.anushka.circlecalc
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class MyCalcTest
+{
+    private lateinit var myCalc: MyCalc
+
+    @Before
+    fun setUp()
+    {
+        myCalc = MyCalc()
+    }
+
+    // subjectUnderTest_actionOrInput_resultState
+    @Test
+    fun calculateCircumference_radiusGiven_returnsCorrectResult()
+    {
+        val result = myCalc.calculateCircumference(2.1)
+        // expecting 13.188
+        assertThat(result).isEqualTo(13.188)
+    }
+
+    @Test
+    fun calculateCircumference_zeroRadiusGiven_returnsCorrectResult()
+    {
+        val result = myCalc.calculateCircumference(0.0)
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun calculateArea_radiusGiven_returnsCorrectResult()
+    {
+        val result = myCalc.calculateArea(2.1)
+        assertThat(result).isEqualTo(13.8474)
+    }
+
+    @Test
+    fun calculateArea_zeroRadiusGiven_returnsCorrectResult()
+    {
+        val result = myCalc.calculateArea(0.0)
+        assertThat(result).isEqualTo(0)
+    }
+}
